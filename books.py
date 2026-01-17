61cc51bd98cee581ca7f7a7589126a4d68591ac6
Michael <michaellittle92@gmail.com>
2026-01-17 22:21:11 +1100

    Start v2 rewrite

--- books.py
+++ books.py
@@ -47,9 +47,6 @@ async def read_author_category_by_query(book_author: str, category:str):
             books_to_return.append(book)
     return books_to_return
 
-
-
-
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
