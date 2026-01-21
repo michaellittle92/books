2da853d177b2fdab462a2c8102793617400e68f6
Michael <michaellittle92@gmail.com>
2026-01-21 12:02:02 +1100

    cleaned up get endpoints and added put endpoint

--- books.py
+++ books.py
@@ -40,8 +40,6 @@ class BookRequest(BaseModel):
         }
     }
 
-
-
 BOOKS = [
     Book(1, 'Computer Science Pro', 'Author One', 'A very nice book', 5),
     Book(2, 'Be Fast with FastAPI', 'Author One', 'A great book', 5),
@@ -55,36 +53,20 @@ BOOKS = [
 async def read_all_books():
     return BOOKS
 
-@app.get("/books/{book_title}")
-async def read_book(book_title: str):
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
+        if book.id == book_id:
             return book
-
+        
 @app.get("/books/")
-async def read_category_by_query(category: str):
+async def read_book_by_rating(book_rating: int):
     books_to_return = []
     for book in BOOKS:
-        if book.get('category').casefold() == category.casefold():
+        if book.rating == book_rating:
             books_to_return.append(book)
     return books_to_return
 
-@app.get("/books/by-author/{author}")
-async def read_books_by_author(author: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('author').casefold() == author.casefold():
-            books_to_return.append(book)
-    return books_to_return
-
-@app.get("/books/{book_author}/")
-async def read_author_category_by_query(book_author: str, category:str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and \
-        book.get('category').casefold() == category.casefold():
-            books_to_return.append(book)
-    return books_to_return
 
 @app.post("/books/create_book")
 async def create_book(book_request: BookRequest):
@@ -98,16 +80,9 @@ def find_book_id(book: Book):
         book.id = 1
     return book
 
+
 @app.put("/books/update_book")
-async def update_book(updated_book=Body()):
+async def update_book(book: BookRequest):
     for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
-            BOOKS[i] = updated_book
-
-@app.delete("/books/delete_book/{book_title}")
-async def delete_book(book_title: str):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
-            BOOKS.pop(i)
-            break
-
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
